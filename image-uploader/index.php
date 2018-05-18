ff457e921d1fef63c25c890cb24be50c4861462e
Ferni <32869982+FerniESP@users.noreply.github.com>
2018-05-18 11:21:42 +0200

    Add files via upload

---
+++ image-uploader/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Uploader</title>
+</head>
+<body>
+
+	<form action="./image-upload.php" method="post" enctype="multipart/form-data">
+		Selecciona la imagen:
+		<input type="file" name="fileToUpload" id="fileToUpload">
+		<input type="submit" value="Upload Image" name="submit">
+	</form>
+
+
+
+</body>
+</html>
